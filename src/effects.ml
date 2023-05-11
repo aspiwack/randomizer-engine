674ff2309826f160e7662e20fe4e1ce2c8efe425
Arnaud Spiwack <arnaud@spiwack.net>
2023-05-11 18:31:09 +0200

    Basic yield effect with a fold_left handler

---
+++ src/effects.ml
@@ -0,0 +1,5 @@
+(** I'm being a little cute here, with naming this sublib @Effects@
+    (plural) while the standard library's sublib is called @Effect@
+    (singular) *)
+
+include Re_effects
---
+++ src/effects/yield.ml
@@ -0,0 +1,52 @@
+(** A small library on using algebraic effects to write
+     generators. Algebraic effects are not stabilised, to the library
+     is kept small in order to easily deal with updates in the
+     future. *)
+
+[@@@alert "-unstable"]
+
+module type Type = sig
+  type t
+end
+
+(** The @Yield@ effect is parameterised by an arbitrary type. Due to
+    effects needing to be monomorphic this parameterisation is done
+    via a functor. It looks a bit silly for @Yield@, but I anticipate
+    that many effects would require more structure of their
+    parameters, and will need to be functors so it's quite uniform
+    this way. *)
+module Make (T: Type) = struct
+
+  (** Type of algebraic-effect based generators. They produce values
+      with [yield], suspending their computations, and are resumed
+      with algebraic handler. *)
+  type t = unit -> unit
+
+  type _ Effect.t += Yield : T.t -> unit Effect.t
+
+  let yield x = Effect.perform @@ Yield x
+
+  module Handler = struct
+    (** Be careful about using handlers directly, as they require the
+    unstable Effect API. *)
+
+    let rec fold_left f current =
+      let open Effect.Shallow in
+      {
+        retc = (fun x -> (current, x));
+        exnc = raise;
+        effc = fun(type b) (eff : b Effect.t) ->
+          match eff with
+          | Yield a ->
+            Some
+              begin
+                fun (k : (b, _) continuation) ->
+                  continue_with k () (fold_left f (f current a))
+              end
+          | _ -> None
+      }
+  end
+
+  let fold_left f a gen =
+    Effect.Shallow.continue_with (Effect.Shallow.fiber gen) () (Handler.fold_left f a)
+end
